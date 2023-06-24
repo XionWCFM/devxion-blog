8ef381f55b4c673ca6da8546b19caf0e144e551a
xion <calf-love@naver.com>
2023-06-25 02:30:26 +0900

    icon setting

---
+++ assets/index.ts
@@ -0,0 +1,59 @@
+import FaceBook from '@/public/icons/facebook.svg';
+import GitHub from '@/public/icons/github.svg';
+import Insta from '@/public/icons/insta.svg';
+import Moon from '@/public/icons/moon.svg';
+import Sun from '@/public/icons/search.svg';
+import Search from '@/public/icons/sun.svg';
+
+interface IconType {
+  src: typeof FaceBook;
+  alt: string;
+  width: number;
+  height: number;
+}
+
+const size = {
+  width: 50,
+  height: 50,
+};
+
+const FaceBookIcon: IconType = {
+  src: FaceBook,
+  alt: 'facebook',
+  ...size,
+};
+const GitHubIcon: IconType = {
+  src: GitHub,
+  alt: 'github',
+  ...size,
+};
+
+const InstaIcon: IconType = {
+  src: Insta,
+  alt: 'insta',
+  ...size,
+};
+const SunIcon: IconType = {
+  src: Sun,
+  alt: 'sun',
+  ...size,
+};
+const MoonIcon: IconType = {
+  src: Moon,
+  alt: 'moon',
+  ...size,
+};
+const SearchIcon: IconType = {
+  src: Search,
+  alt: 'moon',
+  ...size,
+};
+
+export const Icons = {
+  FaceBookIcon,
+  InstaIcon,
+  MoonIcon,
+  SunIcon,
+  SearchIcon,
+  GitHubIcon,
+};
---
+++ components/helper-components/Providers.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface ProvidersProps {}
+
+const Providers = ({}: ProvidersProps) => {
+  return <div>Providers</div>;
+};
+
+export default Providers;
