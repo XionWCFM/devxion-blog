d01c84ad42f97ca2599fc6d057678721ea6b0f7a
xion <calf-love@naver.com>
2023-07-01 16:47:12 +0900

    :iphone: Css change
    change

--- components/ui-components/CardList.tsx
+++ components/ui-components/CardList.tsx
@@ -7,7 +7,7 @@ interface PostCardProps {
   docArray: DocumentTypes[];
 }
 
-const PostCard = ({ docArray }: PostCardProps) => {
+const CardList = ({ docArray }: PostCardProps) => {
   return (
     <div className="smooth">
       <div className=" grid grid-cols-1 lg:grid-cols-2">
@@ -16,16 +16,16 @@ const PostCard = ({ docArray }: PostCardProps) => {
             key={post.title}
             href={`${post.type.toLowerCase()}/${post.slugAsParams}`}
           >
-            <figure className=" flex items-center justify-between rounded-lg px-4 py-2 cursor-pointer mr-4 mt-4 transition-all duration-300 border-2 border-transparent hover:border-yellow-500">
+            <figure className=" flex flex-col md:flex-row md:items-center justify-between rounded-lg px-4 py-2 cursor-pointer mr-4 mt-4 transition-all duration-300 border-2 border-transparent hover:border-yellow-500">
               <div className=" flex flex-col gap-2 overflow-hidden whitespace-nowrap break-words">
-                <h2 className=" font-extrabold text-2xl text-ellipsis ">
+                <h2 className=" font-extrabold text-lg md:text-2xl lg:text-xl text-ellipsis ">
                   {post.title}
                 </h2>
                 <p className=" text-xs text-zinc-400 underline underline-offset-2 pt-1 decoration-dashed">
                   {post.description}
                 </p>
               </div>
-              <div className=" text-xs flex flex-col gap-2 items-end">
+              <div className=" text-xs flex flex-row-reverse md:flex-col gap-2 items-end">
                 <Tag>{post.tags}</Tag>
                 <div className="overflow-hidden whitespace-nowrap">
                   {dateFormat(post.date)}
@@ -39,4 +39,4 @@ const PostCard = ({ docArray }: PostCardProps) => {
   );
 };
 
-export default PostCard;
+export default CardList;
