4d5186b48f6017bef92d877f9ffc1cbc44aef4e9
xion <calf-love@naver.com>
2023-07-01 18:58:15 +0900

    :sparkles: Create fullpage logic and css little change
    more pretty code

--- components/index.ts
+++ components/index.ts
@@ -1,18 +1,16 @@
 // atom
 export { default as Button } from './atom-components/Button';
-export { default as Tag } from './atom-components/Tag';
 export { default as HeadingParagraph } from './atom-components/HeadingParagraph';
 export { default as Paragraph } from './atom-components/Paragraph';
-
+export { default as Tag } from './atom-components/Tag';
+export { default as DarkModeToggleBtn } from './helper-components/DarkModeToggleBtn';
 // helper
 export { default as Mdx } from './helper-components/Mdx';
-export { default as Providers } from './helper-components/Providers';
-export { default as DarkModeToggleBtn } from './helper-components/DarkModeToggleBtn';
 export { default as MoveTop } from './helper-components/MoveTop';
-
+export { default as Providers } from './helper-components/Providers';
+export { default as CardList } from './ui-components/CardList';
+export { default as Footer } from './ui-components/Footer';
 // ui
 export { default as Header } from './ui-components/Header';
-export { default as Footer } from './ui-components/Footer';
-export { default as PostCard } from './ui-components/CardList';
+export { default as MainFullPageComponent } from './ui-components/MainFullPageComponent';
 export { default as PostTitle } from './ui-components/PostTitle';
-export { default as CardList } from './ui-components/CardList';
---
+++ components/ui-components/MainFullPageComponent.tsx
@@ -0,0 +1,100 @@
+'use client';
+
+import { cn } from '@/utils';
+import React, { useEffect } from 'react';
+
+interface MainFullPageComponentProps {}
+
+export interface IPageObj {
+  pageNum: number;
+  bgColor: string;
+}
+
+const pageObjArray = [
+  { pageNum: 1, bgColor: 'bg-[#ffeaa7]' },
+  { pageNum: 2, bgColor: 'bg-[#fab1a0]' },
+  { pageNum: 3, bgColor: 'bg-[#fdcb6e]' },
+  { pageNum: 4, bgColor: 'bg-[#e17055]' },
+];
+
+const MainFullPageComponent = ({}: MainFullPageComponentProps) => {
+  const [windowObj, setWindowObj] = React.useState<Window>();
+  const [currentPageNum, setCurrentPageNum] = React.useState(1);
+  const totalNum = pageObjArray.length;
+  const pageRefs = React.useRef<HTMLDivElement[]>([]);
+
+  React.useEffect(() => {
+    if (window !== undefined) {
+      setWindowObj(window);
+    }
+  }, []);
+
+  const handlePageChange = React.useCallback(
+    (event: Event) => {
+      let scroll = windowObj?.scrollY!;
+      for (let i = 1; i <= totalNum; i++) {
+        if (
+          scroll > pageRefs.current[i].offsetTop - windowObj!.outerHeight / 3 &&
+          scroll <
+            pageRefs.current[i].offsetTop -
+              windowObj!.outerHeight / 3 +
+              pageRefs.current[i].offsetHeight
+        ) {
+          setCurrentPageNum(i);
+          break;
+        }
+      }
+    },
+    [totalNum, windowObj],
+  );
+
+  const handlePointClick = (pageNum: number) => {
+    windowObj?.scrollTo({
+      top: pageRefs.current[pageNum].offsetTop,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    windowObj?.addEventListener('scroll', handlePageChange);
+    return () => {
+      windowObj?.removeEventListener('scroll', handlePageChange);
+    };
+  }, [windowObj, handlePageChange]);
+
+  return (
+    <div>
+      {pageObjArray.map((item, idx) => {
+        return (
+          <div
+            key={idx}
+            ref={(element) => {
+              pageRefs.current[item.pageNum] = element!;
+            }}
+            className={`w-screen h-screen ${item.bgColor}`}
+          >
+            <span>{item.pageNum}</span>
+            {pageObjArray.map((item, idx) => {
+              return (
+                <div
+                  key={item.pageNum}
+                  className={cn(
+                    'rounded-full transition-all w-4 h-4',
+                    currentPageNum === item.pageNum
+                      ? 'bg-black'
+                      : 'bg-gray-400',
+                  )}
+                  onClick={() => {
+                    handlePointClick(item.pageNum);
+                  }}
+                ></div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default MainFullPageComponent;
