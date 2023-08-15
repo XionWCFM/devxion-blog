8bd59624b364ba797f0f01d758bb6c739f65c2c3
xion <calf-love@naver.com>
2023-08-16 04:03:28 +0900

    :sparkles: Customhook logic
    isolate

--- hooks/useTyping.tsx
+++ hooks/useTyping.tsx
@@ -1,5 +1,7 @@
 'use client';
 import React from 'react';
+import useRequestAnimationFrame from './useRequestAnimationFrame';
+import useIntersectionObserver from './useIntersectionObserver';
 
 const useTyping = (sentence: string, ms_delay = 100) => {
   const [word, setWord] = React.useState('');
@@ -7,54 +9,28 @@ const useTyping = (sentence: string, ms_delay = 100) => {
     HTMLDivElement | HTMLParagraphElement | HTMLHeadingElement
   >(null);
   const [currentIndex, setCurrentIndex] = React.useState(0);
-  const lastTimestamp = React.useRef<number | null>(null);
-  React.useEffect(() => {
-    let animationFrameId: number;
-    const animateTyping = (timestamp: number) => {
-      if (lastTimestamp.current === null) lastTimestamp.current = timestamp;
-      const elapsed = timestamp - lastTimestamp.current;
-      if (elapsed > ms_delay) {
-        lastTimestamp.current = timestamp;
-        if (sentence.length > currentIndex) {
-          setWord((state) => {
-            const newState = (state += sentence[currentIndex]);
-            setCurrentIndex((prevIndex) => prevIndex + 1);
-            return newState;
-          });
-        }
-      }
-      animationFrameId = requestAnimationFrame(animateTyping);
-    };
-    animationFrameId = requestAnimationFrame(animateTyping);
 
-    return () => {
-      cancelAnimationFrame(animationFrameId);
-    };
-  }, [currentIndex, ms_delay, sentence]);
-
-  React.useEffect(() => {
-    let observer: IntersectionObserver;
-    const { current } = targetRef;
-
-    if (current) {
-      observer = new IntersectionObserver(
-        (entries) => {
-          if (entries[0].isIntersecting) {
-            setCurrentIndex(0);
-            setWord('');
-          }
-        },
-        { threshold: 0.5 },
-      ); // threshold 값을 조정
-      observer.observe(current);
+  const animateTyping = () => {
+    if (sentence.length > currentIndex) {
+      setWord((state) => {
+        const newState = (state += sentence[currentIndex]);
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+        return newState;
+      });
     }
+  };
+  
+  const intersectHandler = () => {
+    setCurrentIndex(0);
+    setWord('');
+  };
 
-    return () => {
-      if (observer) {
-        observer.disconnect();
-      }
-    };
-  }, []);
+  useRequestAnimationFrame(animateTyping, ms_delay);
+
+  useIntersectionObserver({
+    onIntersect: intersectHandler,
+    target: targetRef,
+  });
 
   return { word, targetRef, setWord };
 };
