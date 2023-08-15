59e243eecfd44a854081559753415fdebb2d4239
xion <calf-love@naver.com>
2023-08-15 16:15:27 +0900

    :sparkles: Typeing
    typing

--- components/fullpage-components/pages/PortfolioMain.tsx
+++ components/fullpage-components/pages/PortfolioMain.tsx
@@ -1,9 +1,19 @@
+import useTyping from '@/hooks/useTyping';
 import React from 'react';
+import PageWrapper from '../PageWrapper';
+import Centering from '@/components/atom-components/Centering';
 
 interface PortfolioMainProps {}
 
 const PortfolioMain = ({}: PortfolioMainProps) => {
-  return <div>PortfolioMain</div>;
+  const { word, targetRef } = useTyping('fdsfdsfdsads');
+  return (
+    <PageWrapper>
+      <Centering col className=" justify-center items-center">
+        <p ref={targetRef}>{word}</p>
+      </Centering>
+    </PageWrapper>
+  );
 };
 
 export default PortfolioMain;
--- hooks/useTyping.tsx
+++ hooks/useTyping.tsx
@@ -1,21 +1,50 @@
+'use client';
 import React from 'react';
 
 const useTyping = (sentence: string, ms_delay = 200) => {
   const [word, setWord] = React.useState('');
+  const targetRef = React.useRef<
+    HTMLDivElement | HTMLParagraphElement | HTMLHeadingElement
+  >(null);
+  const currentIndex = React.useRef(0);
+
+  const handleScroll: IntersectionObserverCallback = React.useCallback(
+    ([entry]) => {
+      if (entry.isIntersecting) {
+        currentIndex.current = 0;
+        setWord('');
+      }
+    },
+    [],
+  );
+
   React.useEffect(() => {
-    let currentIndex = 0;
     const interval = setInterval(() => {
-      if (sentence.length > currentIndex) {
+      if (sentence.length > currentIndex.current) {
         setWord((state) => {
-          currentIndex += 1;
-          return (state += sentence[currentIndex]);
+          const newState = (state += sentence[currentIndex.current]);
+          currentIndex.current += 1;
+          return newState;
         });
       }
     }, ms_delay);
 
-    return () => clearTimeout(interval);
+    return () => {
+      clearTimeout(interval);
+    };
   }, [ms_delay, sentence]);
-  return word;
+
+  React.useEffect(() => {
+    let observer: IntersectionObserver;
+    const { current } = targetRef;
+    if (current) {
+      observer = new IntersectionObserver(handleScroll, { threshold: 0.7 });
+      observer.observe(current);
+    }
+
+    return () => observer && observer.disconnect();
+  }, [handleScroll]);
+  return { word, targetRef, setWord };
 };
 
 export default useTyping;
