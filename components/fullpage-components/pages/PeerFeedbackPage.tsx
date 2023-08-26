d6e5fcbe5fdf577485d1c3b28f96b1959dc7e691
devgiljong <calf-love@naver.com>
2023-08-26 14:40:47 +0900

    :recycle: Animation patch
    patch

--- components/fullpage-components/pages/PeerFeedbackPage.tsx
+++ components/fullpage-components/pages/PeerFeedbackPage.tsx
@@ -4,7 +4,6 @@ import FadeInProvider from '@/components/helper-components/FadeInProvider';
 import { PeerFeedback } from '@/types/resume';
 import HeadingParagraph from '@/components/atom-components/HeadingParagraph';
 import HighlightSentence from '@/components/ui-components/HighlightSentence';
-import Paragraph from '@/components/atom-components/Paragraph';
 import LineBreakParagraph from '@/components/helper-components/LineBreakParagraph';
 
 interface PeerFeedbackProps {
@@ -23,7 +22,7 @@ const PeerFeedbackPage = ({ peer }: PeerFeedbackProps) => {
               key={idx}
               className=" border-l-2 border-yellow-500 border-dotted pl-3 py-3 hover:brightness-110 hover:scale-105 transition-all duration-200 rounded-md"
             >
-              <div className=" flex gap-x-2 text-yellow-500 dark:text-yellow-400">
+              <div className=" flex gap-x-2 text-yellow-500 dark:text-yellow-400 text-base lg:text-xl mb-4">
                 <span className=" font-extrabold">{item.belong}</span>
                 <span>{item.name}</span>
                 <span>{item.position}</span>
--- components/fullpage-components/pages/PortfolioMain.tsx
+++ components/fullpage-components/pages/PortfolioMain.tsx
@@ -2,11 +2,7 @@
 import useTyping from '@/hooks/useTyping';
 import React from 'react';
 import PageWrapper from '../PageWrapper';
-import Centering from '@/components/atom-components/Centering';
-import * as Popover from '@radix-ui/react-popover';
 import { lineBreak } from '@/utils';
-import Paragraph from '@/components/atom-components/Paragraph';
-import LineBreakParagraph from '@/components/helper-components/LineBreakParagraph';
 
 interface PortfolioMainProps {}
 
@@ -14,13 +10,6 @@ const PortfolioMain = ({}: PortfolioMainProps) => {
   const sentence =
     '안녕하세요. \n 저는 프론트엔드 개발자 유길종입니다. \n 제 장점은 항상 왜?를 먼저 생각하는 습관 입니다. \n ';
   const portfolio = useTyping(sentence);
-  const [isOpen, setIsOpen] = React.useState(false);
-  const handleMouseEnter = () => {
-    setIsOpen(true);
-  };
-  const handleMouseLeave = () => {
-    setIsOpen(false);
-  };
 
   return (
     <PageWrapper centering>
@@ -38,24 +27,11 @@ const PortfolioMain = ({}: PortfolioMainProps) => {
         ))}
       </div>
       <div className=" absolute bottom-10">
-        <div className=" flex smooth justify-center items-center flex-col gap-y-4">
-          <Popover.Root open={isOpen} onOpenChange={setIsOpen}>
-            <Popover.Trigger
-              className=" smooth border-none "
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
-            >
-              <div className=" text-zinc-600 dark:text-zinc-400">
-                <div className="">Learn more about what i do</div>
-                <div className=" text-5xl">▿</div>
-              </div>
-            </Popover.Trigger>
-            <Popover.Portal>
-              <Popover.Content className=" border-none text-zinc-400 text-sm">
-                스크롤을 내려보세요!
-              </Popover.Content>
-            </Popover.Portal>
-          </Popover.Root>
+        <div className=" flex smooth animate-bounce justify-center items-center flex-col gap-y-4">
+          <div className=" text-zinc-600 dark:text-zinc-400">
+            <div className="">Learn more about what i do</div>
+            <div className=" text-5xl text-center">▿</div>
+          </div>
         </div>
       </div>
     </PageWrapper>
